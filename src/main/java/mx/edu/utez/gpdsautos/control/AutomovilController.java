cf53c988d4fcd1b3d578e4e818d2429feb783a3e
Marianne Santos <20213tn140@utez.edu.mx>
2024-12-04 18:28:42 -0600

    [UPDATE] automovil controller

--- src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
+++ src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/autos")
+@RequestMapping("/api/carros")
 public class AutomovilController {
 
     @Autowired
